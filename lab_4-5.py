947012aef91405de9befdabf8b2ea1bacc29a35e
Rrinvil <144148135+Rrinvil@users.noreply.github.com>
2023-10-23 09:01:48 -0400

    Add files via upload

---
+++ lab_4-5.py
@@ -0,0 +1,6 @@
+person1 = input("enter your first name")
+
+person2Ryan = input("enter another person's first name")
+
+output_string = "hello, {}. My name is {}.". format(person1, person2)
+print(output_string)
